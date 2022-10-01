4c2f623df61f3a69eecf004dd5941f9d1aa84687
Andres <Andres@LAPTOP-J2FV3IBE>
2022-10-01 17:36:40 -0500

    Web Page - Actualizado 01-10-2022 Funciona las paginas de productos Editar y Eliminar No funciona el Css para podificar.html

--- src/main/java/com/co/Control.java
+++ src/main/java/com/co/Control.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -42,11 +43,28 @@ public class Control {
     public String productos(){
         return "productos";
     }
+    @GetMapping("/productosperro")
+    public String productosperro(){
+        return "productosperro";
+    }
     
     @PostMapping("/guardar")
     public String guardar(@ModelAttribute Usuario usuarios){
         usuarioDao.save(usuarios);
         return "redirect:/loginadmin";
+    }
+    
+    @GetMapping("/editar/{idusuario}")
+    public String editar(@PathVariable("idusuario") Integer idusuario, Model model){
+        var usuarios= usuarioDao.findById(idusuario);
+        model.addAttribute("usuarios",usuarios);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar/{idusuario}")
+    public String eliminar(@PathVariable("idusuario") Integer idusuario){
+        usuarioDao.deleteById(idusuario);
+          return "redirect:/loginadmin";
     }
     
        
